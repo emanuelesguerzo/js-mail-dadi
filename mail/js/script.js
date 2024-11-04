2d6fe383e310d99c13680dd5e83d3681e54b4ae1
emanuelesguerzo <155169753+emanuelesguerzo@users.noreply.github.com>
2024-11-04 13:50:58 +0100

    feat: mail exercise complete

---
+++ mail/js/script.js
@@ -0,0 +1,17 @@
+// MAIL
+const mails = ["pippo@disney.com", "pluto@disney.com", "paperino@disney.com"];
+const userMail = prompt("Inserisci la tua mail!");
+let accessGranted = false;
+
+for (let i = 0; i < mails.length; i++) {
+    if (userMail === mails[i]) {
+        accessGranted = true;
+        break;
+    }
+}
+
+if (accessGranted === true) {
+    console.log("Accesso eseguito! Bentornato!");
+} else {
+    console.log("Accesso Negato! Controlla se hai scritto la mail correttamente e riprova.");
+}
